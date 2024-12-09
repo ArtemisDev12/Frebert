098fced18debfc6326fee6be6527d7932b3e82d9
ArtemisDev12 <forgety42@gmail.com>
2024-12-09 14:42:10 +0700

    add and change Example2.java

---
+++ Example2.java
@@ -0,0 +1,18 @@
+/* 
+Еще один короткий пример. 
+Назовите этот файл Exarnple2.java . 
+*/
+class Example2 {
+	public static void main(String[] args) {
+		int num; // объявление переменной по имени num
+		
+		num = 100; // присваивание переменной nurn значения 100
+
+		System.out.println("Значение пurn: " + num);
+
+		num = num * 2;
+
+		System.out.print("Знaчeниe num * 2: ");
+		System.out.println(num);
+	}
+}

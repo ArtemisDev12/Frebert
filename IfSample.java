78564d491bb2d93d41138d34047b5d66ee993dc0
ArtemisDev12 <forgety42@gmail.com>
2024-12-09 14:53:35 +0700

    add and change IfSample.java

---
+++ IfSample.java
@@ -0,0 +1,18 @@
+/*
+Демонстрация работы оператора if. 
+Назовите этот файл IfSample.java. 
+*/
+class IfSample {
+	public static void main(String[] args) {
+		int x, y;
+		x = 10;
+		y = 20;
+		if (x < y) System.out.println ("Знaчeниe х меньше у");
+		x = x * 2;
+		if (x == y) System.out .println("Tenepь значение х равно у");
+		x = x * 2;
+		if (x > y) System.out.println("Teпepь значение х больше у");
+		// Здесь ничего не отобразится 
+		if (x == y) System.out.println( "Этoт вывод вы не увидите");
+	}
+}
